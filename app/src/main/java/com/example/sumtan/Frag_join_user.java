12d03498f6044eaa1bd1f1b6be8a6179e3fa9b3f
chohyeryung <s2019w38@e-mirim.hs.kr>
2020-09-28 02:47:16 +0900

    UI 정리

---
+++ app/src/main/java/com/example/sumtan/Frag_join_user.java
@@ -0,0 +1,34 @@
+package com.example.sumtan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Frag_join_user extends Fragment {
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        view = inflater.inflate(R.layout.activity_frag_join_user,container,false);
+
+        Button btn_join_user=(Button)view.findViewById(R.id.btn_join_user);
+        btn_join_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), MainActivity.class));
+            }
+        });
+
+        return view;
+    }
+}
